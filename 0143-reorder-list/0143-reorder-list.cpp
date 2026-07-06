381b41483b5ab0630bca220a658cc2944189b6f8
Mohit Kumar <151816175+mohittt-kumar@users.noreply.github.com>
2026-07-06 16:16:39 +0530

    Time: 0 ms (100%), Space: 22.9 MB (54.07%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -8,51 +8,38 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if (!head || !head->next)
-            return;
-        ListNode *slow = head, *fast = head;
-        while (fast->next && fast->next->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+        ListNode *slow=head;
+        ListNode* fast=head;
+
+        ListNode* prev=NULL;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
+        ListNode *curr=slow->next;
+        slow->next=NULL;
 
-        ListNode *prev = nullptr;
-        ListNode *curr = slow->next;
-        slow->next = nullptr;
-
-        while (curr) {
-            ListNode *nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
+        //reverse second list;
+        while(curr){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
         }
+        ListNode * first=head;
+        ListNode* second=prev;
 
-        ListNode *first = head;
-        ListNode *second = prev;
+        while(second){
+            ListNode * temp1=first->next;
+            ListNode* temp2=second->next;
+            first->next=second;
+            second->next=temp1;
 
-        while (second) {
-            ListNode *temp1 = first->next;
-            ListNode *temp2 = second->next;
-
-            first->next = second;
-            second->next = temp1;
-
-            first = temp1;
-            second = temp2;
+            second=temp2;
+            first=temp1;
         }
     }
 };
