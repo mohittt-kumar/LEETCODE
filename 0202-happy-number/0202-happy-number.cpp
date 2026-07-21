220a2cc6b50d2f751fa5ee0ab8c9e96df11f48ee
Mohit Kumar <151816175+mohittt-kumar@users.noreply.github.com>
2026-07-21 15:29:23 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.29%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int rem;
+        int sum=0;
+        if(n==1||n==7)return true;
+        if(n<10){
+            return false;
+        }
+        int x=n;
+        while(n){
+            rem=n%10;
+            sum+=rem*rem;
+            n=n/10;
+        }
+        return isHappy(sum);
+    }
+};
