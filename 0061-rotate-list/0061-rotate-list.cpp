bccaa163dae2e5cac77f3c77f37336cfd2472bd7
Mohit Kumar <151816175+mohittt-kumar@users.noreply.github.com>
2026-07-27 19:43:09 +0530

    Time: 0 ms (100%), Space: 16.4 MB (64.82%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,40 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k==0)return head;
-
-        ListNode* tail=head;
-        int n=1;
-        while(tail->next){
-            tail=tail->next;
-            n++;
+    ListNode * reverse(ListNode *head){
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        while(curr){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
         }
-        k=k%n;
-        if(k==0)return head;
-        tail->next=head;
-
-        ListNode* newtail=head;
-        for(int i=1;i<n-k;i++){
-            newtail=newtail->next;
-        }
-        
-        ListNode* newhead=newtail->next;
-        newtail->next=NULL;
-
-        return newhead;
-        
+        return prev;
     }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL || k == 0)
+        return head;
+        ListNode * temp=head;
+        int n=0;
+        while(temp){
+            n++;
+            temp=temp->next;
+        }
+        ListNode * curr=head;
+        ListNode * prev=NULL;
+        k=k%n;
+        int count=n-k;
+        while(count>0){
+            prev=curr;
+            curr=curr->next;
+            count--;
+        }
+        prev->next=NULL;
+        temp=head;
+        temp=reverse(temp);
+        curr=reverse(curr);
+
+        head=temp;
+
+        while(temp->next){
+            temp=temp->next;
+        }
+
+        temp->next=curr;
+
+
+        return reverse(head);
+    }
+
 };
