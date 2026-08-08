86742f19e281c083eb9bdd64352a1d6cc8a51d2e
Mohit Kumar <151816175+mohittt-kumar@users.noreply.github.com>
2026-08-08 17:47:41 +0530

    Time: 0 ms (100%), Space: 8 MB (98.16%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(int i=0;i<word.size();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                 count++;
+            }
+        }return count==0 || count==word.size() ||(count==1 && word[0]>='A' && word[0]<='Z');
+    }
+};
