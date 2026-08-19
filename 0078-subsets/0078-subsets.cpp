fc2fd4da3779e5eb221b4e981525e2d48ef1f913
Mohit Kumar <151816175+mohittt-kumar@users.noreply.github.com>
2026-08-19 13:59:18 +0530

    Time: 5 ms (3.51%), Space: 17 MB (5.01%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void subseq(vector<int>nums,int index, vector<vector<int>>&ans,vector<int>temp){
+        if(index==nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        subseq(nums,index+1,ans,temp);
+        temp.push_back(nums[index]);
+        subseq(nums,index+1,ans,temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        subseq(nums,0,ans,temp);
+
+        return ans;
+    }
+};
